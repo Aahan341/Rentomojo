f4ee2660ebb535abbb373de2a5251fbbd6d74721
Suhail <aaahan006@gmail.com>
2023-07-13 18:25:56 +0530

    pages

--- Components/navbar.js
+++ Components/navbar.js
@@ -21,4 +21,27 @@ const navbar = ()=>{
     ` 
 };
 
-export  {navbar};
+const nav2=()=>{
+    return `
+    <div id="main">
+    <div id="part1">
+        <a href="index.html">Home</a>
+        <p>&gt;</p>
+        <p> Packages</p>
+     </div>
+     <div id="part2">
+        <ul>
+            <li class="pack"><a href="#">Packages</a></li>
+            <li><a href="#">Furniture</a></li>
+            <li><a href="#">Appliances</a></li>
+            <li><a href="#">Electronics</a></li>
+            <li><a href="#">Fitness</a></li>
+        </ul>
+    </div>       
+    </div>
+    `
+}
+
+
+
+export  {navbar,nav2};
---
+++ scripts/fitness.js
@@ -0,0 +1,3 @@
+import { navbar } from "../Components/navbar.js";
+let nav=document.getElementById('navbar')
+nav.innerHTML=navbar();
